ead96f290f7257742a7ae2d51e943508129346f5
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-03-02 00:00:59 +0530

    add avatar

--- Frontend/password-manager/src/component/Signup/Signup.jsx
+++ Frontend/password-manager/src/component/Signup/Signup.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
-import { TextField, Button, Grid, Typography, makeStyles } from '@material-ui/core';
+import { Link, TextField, Button, Grid, Typography, makeStyles, Container, Avatar } from '@material-ui/core';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '80%',
+
     },
   },
+  avatarSection: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  avatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  }
+
 }));
 
 const Signup = () => {
   const classes = useStyles();
   const [formData, setFormData] = useState({
+    avatar: null,
     firstName: '',
     lastName: '',
     email: '',
@@ -32,59 +45,102 @@ const Signup = () => {
     // Handle form submission here, for example, send formData to server
     console.log(formData);
   };
+
+  const handleAvatarChange = (event) => {
+    const file = event.target.files[0];
+    setFormData ({
+      ...formData,
+      avatar: file
+    })
+  }
   return (
-    <div>
-      <form className={classes.root} onSubmit={handleSubmit}>
-        <Grid container direction="column" spacing={2}>
-          <Grid item>
-            <Typography variant="h6">Registration Form</Typography>
-          </Grid>
-          <Grid item>
-            <TextField
-              label="First Name"
-              variant="outlined"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item>
-          <TextField
-              label="Last Name"
-              variant="outlined"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              label="Email"
-              variant="outlined"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              label="Password"
-              variant="outlined"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary" type="submit">
-              Submit
-            </Button>
-          </Grid>
+
+    <Container maxWidth="sm">
+      <Grid spacing={2} justifyContent='center'>
+        <Grid item >
+          <form className={classes.root} onSubmit={handleSubmit} style={{ border: '2px solid red' }}>
+            <Grid container direction="column" spacing={2}>
+
+              <Grid item>
+                <Typography variant="h4">Sign UP</Typography>
+                <Typography variant='subtitle1'>Create a new user</Typography>
+              </Grid>
+              <Grid item className={classes.avatarSection}>
+                <label htmlFor="avatar-upload">
+                  <Avatar 
+                  alt="profile" 
+                  className={classes.avatar}
+                  src={formData.avatar ? URL.createObjectURL(formData.avatar): ""} />
+                </label>
+                <input 
+                type="file"
+                accept='image/'  
+                id='avatar-upload' 
+                style={{display: 'none'}}
+                onChange={handleAvatarChange}/>
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="First Name"
+                  variant="outlined"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Last Name"
+                  variant="outlined"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+
+              </Grid>
+
+              <Grid item>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary" type="submit">
+                  Sign Up
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary" type="submit">
+                  Sign Up with Google
+                </Button>
+              </Grid>
+              <Grid item>
+
+                <Typography variant='p'>Click here to?
+                  <Link> Log In</Link></Typography>
+
+              </Grid>
+            </Grid>
+          </form>
         </Grid>
-      </form>
-    </div>
+      </Grid>
+    </Container>
+
   )
 }
 
